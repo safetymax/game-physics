09db6698ff5bebfdc8314bc47f46b22c01c43073
Max <maximilian.grummt@gmx.de>
2026-06-25 14:42:18 +0200

    enemy move

--- enemy_move.gd
+++ enemy_move.gd
@@ -4,6 +4,7 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 #add list of node3ds as a path to walk up and down, and a variable to keep track of the current target node3d
+@export
 var path: Array[Node3D] = []
 var current_target_index: int = 0
 
@@ -22,9 +23,9 @@ func _physics_process(delta: float) -> void:
 
 	velocity = forward.normalized() * SPEED
 	#if close enough to the target node, stop moving, turn 360 degrees, and then move to the next node
-	if forward.length() < 0.1:
-		velocity = Vector3.ZERO
+	#if forward.length() < 0.1:
+		#velocity = Vector3.ZERO
 		#turn 360 degrees
-		rotate_y(deg_to_rad(360) * delta)
+		#rotate_y(deg_to_rad(360) * delta)
 
 	move_and_slide()
