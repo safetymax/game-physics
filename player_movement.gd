afac6b76e606fdbc42a6d25bad3a772799bb32fd
Max <maximilian.grummt@gmx.de>
2026-06-26 15:05:56 +0200

    player

--- player_movement.gd
+++ player_movement.gd
@@ -4,6 +4,10 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
+func _ready() -> void:
+	#lock the mouse cursor to the center of the screen
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	pass
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
